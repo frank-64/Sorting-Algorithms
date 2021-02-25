336b386f38a4e1439b87ca398b20a6751e3a9641
frank-64 <fcminipad@gmail.com>
2021-02-25 14:24:37 +0000

    FCFS started.

--- FCFS.c
+++ FCFS.c
@@ -7,9 +7,14 @@ struct job
     int arrival_time;
 };
 
-int first_come_first_serve(struct job jobs[]);
+int first_come_first_serve(struct job jobs[], int len);
 
 
-int first_come_first_serve(struct job jobs[]){
-
+int first_come_first_serve(struct job jobs[], int len){
+    int current_time = 0;
+    for (int i = 0; i < len; ++i) {
+        current_time = current_time + jobs[i].run_time;
+        printf("Job %c was processed. Finish time: %d\n", jobs[i].id,current_time);
+    }
+    return current_time;
 }
--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 int main(){
     // First array of jobs
     struct job jobs1[] = {{'A',8,0, },{'B',4,0}, {'C',4,0}, {'D',4,0}};
-
+    int len = 4;
     for (int i = 0; i < 4; ++i) {
         printf("Job: %c, Duration: %d, Arrival Time: %d \n", jobs1[i].id, jobs1[i].run_time, jobs1[i].arrival_time);
     }
-    int FCFS_time = first_come_first_serve(jobs1);
+    printf("\n");
+    int FCFS_time = first_come_first_serve(jobs1, len);
+    printf("\n");
+    printf("Total time for all jobs: %d\n", FCFS_time);
+    printf("Mean time for the jobs: %f\n", (float) FCFS_time/len);
 }
